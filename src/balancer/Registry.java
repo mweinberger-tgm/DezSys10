e064dad56417e4a079d581b55e72ca07e0b59561
mweinberger-tgm <mweinberger@student.tgm.ac.at>
2016-03-04 00:31:16 +0100

    ~

--- src/balancer/Registry.java
+++ src/balancer/Registry.java
@@ -3,7 +3,7 @@ package balancer;
 
 import server.ServerInfo;
 
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * Created by Michael on 03.03.2016.
@@ -12,10 +12,10 @@ import java.util.Map;
 public class Registry {
 
     private static Registry instance = null;
-    private Map serverlist;
+    private ArrayList<ServerInfo> serverlist;
 
     private Registry() {
-
+        this.serverlist = new ArrayList<>();
     }
 
     public static synchronized Registry getInstance() {
@@ -26,14 +26,22 @@ public class Registry {
     }
 
     public void addServer(ServerInfo server) {
-        this.serverlist.put(server.getHash(), server);
+        this.serverlist.add(server);
     }
 
     public void removeServer(ServerInfo server) {
-        this.serverlist.remove(server.getHash());
+        this.serverlist.remove(server);
     }
 
-    public Map getMap() {
+    public ArrayList<ServerInfo> getList() {
         return this.serverlist;
+    }
+
+    public String toString() {
+        String out = "";
+        for (int i = 0; i < this.serverlist.size(); i++) {
+            out += "" + this.serverlist.get(i).getIp() + ":" + this.serverlist.get(i).getPort() + "\n";
+        }
+        return out;
     }
 }
